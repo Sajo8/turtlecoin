c6d01e288c3677666c9c54c82d544a25242edf9c
Ehsan Afraz <46084703+bitrial@users.noreply.github.com>
2018-12-28 23:33:52 +0330

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -4,24 +4,7 @@
 
 #pragma once
 
-const std::string nonWindowsAsciiArt = 
-      "\n                                                                                                                               \n"
-	    "BBBBBBBBBBBBBBBBB   IIIIIIIIIITTTTTTTTTTTTTTTTTTTTTTTRRRRRRRRRRRRRRRRR   IIIIIIIIII               AAA               LLLLLLLLLLL\n"
-        "B::::::::::::::::B  I::::::::IT:::::::::::::::::::::TR::::::::::::::::R  I::::::::I              A:::A              L:::::::::L\n"
-        "B::::::BBBBBB:::::B I::::::::IT:::::::::::::::::::::TR::::::RRRRRR:::::R I::::::::I             A:::::A             L:::::::::L\n"
-        "BB:::::B     B:::::BII::::::IIT:::::TT:::::::TT:::::TRR:::::R     R:::::RII::::::II            A:::::::A            LL:::::::LL\n"
-        "  B::::B     B:::::B  I::::I  TTTTTT  T:::::T  TTTTTT  R::::R     R:::::R  I::::I             A:::::::::A             L:::::L\n"
-        "  B::::B     B:::::B  I::::I          T:::::T          R::::R     R:::::R  I::::I            A:::::A:::::A            L:::::L\n"
-        "  B::::BBBBBB:::::B   I::::I          T:::::T          R::::RRRRRR:::::R   I::::I           A:::::A A:::::A           L:::::L\n"
-		"  B:::::::::::::BB    I::::I          T:::::T          R:::::::::::::RR    I::::I          A:::::A   A:::::A          L:::::L\n"               
-        "  B::::BBBBBB:::::B   I::::I          T:::::T          R::::RRRRRR:::::R   I::::I         A:::::A     A:::::A         L:::::L\n"               
-        "  B::::B     B:::::B  I::::I          T:::::T          R::::R     R:::::R  I::::I        A:::::AAAAAAAAA:::::A        L:::::L\n"               
-        "  B::::B     B:::::B  I::::I          T:::::T          R::::R     R:::::R  I::::I       A:::::::::::::::::::::A       L:::::L\n"               
-        "  B::::B     B:::::B  I::::I          T:::::T          R::::R     R:::::R  I::::I      A:::::AAAAAAAAAAAAA:::::A      L:::::L         LLLLLL\n"
-        "BB:::::BBBBBB::::::BII::::::II      TT:::::::TT      RR:::::R     R:::::RII::::::II   A:::::A             A:::::A   LL:::::::LLLLLLLLL:::::L\n"
-        "B:::::::::::::::::B I::::::::I      T:::::::::T      R::::::R     R:::::RI::::::::I  A:::::A               A:::::A  L::::::::::::::::::::::L\n"
-        "B::::::::::::::::B  I::::::::I      T:::::::::T      R::::::R     R:::::RI::::::::I A:::::A                 A:::::A L::::::::::::::::::::::L\n"
-        "BBBBBBBBBBBBBBBBB   IIIIIIIIII      TTTTTTTTTTT      RRRRRRRR     RRRRRRRIIIIIIIIIIAAAAAAA                   AAAAAAALLLLLLLLLLLLLLLLLLLLLLLL\n";
+const std::string windowsAsciiArt = "\n";
 
 const std::string nonWindowsAsciiArt = "\n";
 
